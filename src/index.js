43088c6acc098aa65c06a018b516d8dfa920abaf
fergalhennessy <fergalhennessy@berkeley.edu>
2024-01-08 07:51:44 -0600

    added new tempate

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import Demo from "./Demo";
-import registerServiceWorker from "./registerServiceWorker";
-
-ReactDOM.render(<Demo />, document.getElementById("root"));
-registerServiceWorker();
